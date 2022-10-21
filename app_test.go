6401d3141b2825331eaf11567ba1bca9a714b1dd
Aleksey Bakin <alexey.bakin@gmail.com>
2022-10-21 21:06:07 +0400

    refactor: use suite

--- app_test.go
+++ app_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/demo/app"
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 //go:generate mockgen -destination=mock_db_test.go -package=app_test . UsersDB
@@ -29,41 +29,49 @@ func (u userMatcher) String() string {
 	return fmt.Sprintf("is equal to %+v", u.User)
 }
 
-func TestDuplicateUser(t *testing.T) {
-	mockCtl := gomock.NewController(t)
-	mockDB := NewMockUsersDB(mockCtl)
-	appInst := app.New(mockDB)
+type AppSuite struct {
+	suite.Suite
+	mockDB  *MockUsersDB
+	appInst *app.App
+}
 
+func (s *AppSuite) SetupTest() {
+	mockCtl := gomock.NewController(s.T())
+	s.mockDB = NewMockUsersDB(mockCtl)
+	s.appInst = app.New(s.mockDB)
+}
+
+func (s *AppSuite) TestDuplicateUser() {
 	user := app.User{
 		ID:    "test_user_1",
 		Name:  "test_name_1",
 		Phone: "8-911-234-4567",
 	}
 
-	mockDB.EXPECT().FindUser(user.ID).Return(user, nil)
-	mockDB.EXPECT().AddUser(userMatcher{user}).Return(nil)
-	newID, err := appInst.DuplicateUser(user.ID)
+	s.mockDB.EXPECT().FindUser(user.ID).Return(user, nil)
+	s.mockDB.EXPECT().AddUser(userMatcher{user}).Return(nil)
+	newID, err := s.appInst.DuplicateUser(user.ID)
 
-	require.NoError(t, err)
-	require.NotEqual(t, newID, user.ID)
+	s.Require().NoError(err)
+	s.Require().NotEqual(newID, user.ID)
 }
 
 var errAddUser = errors.New("test error")
 
-func TestDuplicateErr(t *testing.T) {
-	mockCtl := gomock.NewController(t)
-	mockDB := NewMockUsersDB(mockCtl)
-	appInst := app.New(mockDB)
-
+func (s *AppSuite) TestDuplicateErr() {
 	user := app.User{
 		ID:    "test_user_1",
 		Name:  "test_name_1",
 		Phone: "8-911-234-4567",
 	}
 
-	mockDB.EXPECT().FindUser(user.ID).Return(user, nil)
-	mockDB.EXPECT().AddUser(gomock.Any()).Return(errAddUser)
-	_, err := appInst.DuplicateUser(user.ID)
+	s.mockDB.EXPECT().FindUser(user.ID).Return(user, nil)
+	s.mockDB.EXPECT().AddUser(gomock.Any()).Return(errAddUser)
+	_, err := s.appInst.DuplicateUser(user.ID)
 
-	require.ErrorIs(t, err, errAddUser)
+	s.Require().ErrorIs(err, errAddUser)
+}
+
+func TestApp(t *testing.T) {
+	suite.Run(t, new(AppSuite))
 }
